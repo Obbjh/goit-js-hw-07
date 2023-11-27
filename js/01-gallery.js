ce92b851164c68331a1b3909702ae8f141606c00
Losev Vladyslav <135152001+Obbjh@users.noreply.github.com>
2023-11-27 19:11:25 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,68 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+//Створення розмітки 
+
+const galleryList = document.querySelector(".gallery")
+
+
+const galleryCurrent = galleryItems.map(({preview, description, original}) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+  `).join('');
+
+  galleryList.insertAdjacentHTML('afterbegin', galleryCurrent);
+
+// Делегування Gallery та заміна img при кліку
+
+galleryList.addEventListener(`click`, onclick);
+
+function onclick(evt) {
+    if (evt.target.nodeName !== `IMG`) {
+        return;
+    }
+
+    evt.preventDefault();
+
+    const originalSrc = evt.target.dataset.source;
+    const description = evt.target.alt;
+
+    openLightbox(originalSrc, description);
+}
+
+// створення basicLightbox для img
+
+function openLightbox(src, alt) {
+    document.addEventListener('keydown', closeOnEsc);
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img
+                src="${src}"
+                alt="${alt}"
+                class="gallery__image"
+            />
+        </div>
+    `);
+
+    instance.show();
+
+      function closeOnEsc(event) {
+        if (event.key === 'Escape') {
+            instance.close(() => {
+                document.removeEventListener('keydown', closeOnEsc);
+            });
+        }
+    }
+
+    return instance;
+}
+
