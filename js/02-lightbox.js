949cb13e950853b42ecd39ca5061791ee2915fd3
Losev Vladyslav <135152001+Obbjh@users.noreply.github.com>
2023-11-28 17:45:31 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+//Створення розмітки 
+
+const galleryList = document.querySelector(".gallery")
+
+const galleryCurrent = galleryItems.map(({preview, description, original}) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+  `).join('');
+
+galleryList.insertAdjacentHTML('afterbegin', galleryCurrent);
+  
+//Використання simplelightbox
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
